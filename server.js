12073bba586647e58131bbcf2be2af66980909c0
LindsayPeltier <lindsaypeltier@gmail.com>
2019-11-12 05:42:03 -0800

    basic server set up / Heroku deployment

---
+++ server.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Application Middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser());
+
+// Set the view engine for server-side templating
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// API Routes
+// Renders the search form
+app.get('/', newSearch);
+
+// Creates a new search to the Google Books API
+app.post('/searches', createSearch);
+
+// Catch-all
+app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+// HELPER FUNCTIONS
+// Only show part of this to get students started
+function Book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+
+  this.title = info.title || 'No title available';
+
+}
+
+// Note that .ejs file extension is not required
+function newSearch(request, response) {
+  response.render('pages/index');
+}
+
+// No API key required
+// Console.log request.body and request.body.search
+function createSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  // console.log(request.body);
+  // console.log(request.body.search);
+
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', {searchResults: results}));
+  // .then(results => console.log(results));
+
+  // how will we handle errors?
+}
