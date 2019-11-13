97892b4dcd8dae2969521cb848afb74ce34b47de
LindsayPeltier <lindsaypeltier@gmail.com>
2019-11-13 12:11:17 -0800

    starter code

--- server.js
+++ server.js
@@ -1,73 +1,78 @@
 'use strict';
 
-// App Dependencies
+// Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
-require('dotenv').config();
-const pg = require('pg');
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Application Middleware
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-// Database Setup
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
 
 // API Routes
-// Renders the search form
-app.get('/', newSearch);
-
-// Creates a new search to the Google Books API
+app.get('/', getBooks) //define route to get all books
+app.get('/searches/new', newSearch);
 app.post('/searches', createSearch);
+app.post('/books', createBook)
+app.get('/books/:id', getOneBook);
 
-// Catch-all
+
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-// CALLBACK HELPER FUNCTIONS
+// HELPER FUNCTIONS
 function Book(info) {
-  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpeg'
-  let httpRegex = /^(http:\/\/)/g;
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  let httpRegex = /^(http:\/\/)/g
 
-  this.title = info.title || 'No title available'
-  this.author = info.author? info.authors[0]: 'No author available'
-  this.isbn = info.industryIdentifiers? info.industryIdentifiers[0].identifier: 'No ISBN Number'
-  this.image_url = info.imageLinks? info.imageLinks.smallThumbnail.replace(httpRegex, 'https://'): placeholderImage
-  this.description = info.description || 'No description available';
-  this.id = info.industryIdentifiers? info.industryIdentifiers[0].identifier: ''
+  this.title = info.title ? info.title : 'No title available';
+  this.author = info.authors ? info.authors[0] : 'No author available';
+  this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
+  this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail.replace(httpRegex, 'https://') : placeholderImage;
+  this.description = info.description ? info.description : 'No description available';
+  this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
 }
 
-// Note that .ejs file extension is not required
 function newSearch(request, response) {
   response.render('pages/index');
 }
 
-// No API key required
-// Console.log request.body and request.body.search
 function createSearch(request, response) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-
-  // console.log(request.body);
-  // console.log(request.body.search);
 
   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
 
   superagent.get(url)
     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => response.render('pages/searches/show', {searchResults: results}))
-    .catch(err => handleErrors(err, response));
+    .then(results => response.render('pages/searches/show', { searchResults: results }))
+    .catch(err => handleError(err, response));
 }
 
-function handleErrors(error, response){
-  response.render('pages/error', {error: error})
+function getBooks() {
+  //create a SQL statement to get all books in the the database that was saved previously
+  //render the books on an EJS page
+  //catch any errors
+}
+
+function createBook(){
+  //create a SQL statement to insert book
+  //return id of book back to calling function
+
+}
+
+function getOneBook(){
+  //use the id passed in from the front-end (ejs form) 
+
+}
+
+function handleError(error, response) {
+  response.render('pages/error', { error: error });
 }
